df01ee7660a0b5a7bc249fdce8749630c0f32bfa
ODN MAN <oduntane34.5@gmail.com>
2022-06-28 20:24:18 +0100

    Maven Package

---
+++ src/main/java/com/odnman/calculator/Calculator.java
@@ -0,0 +1,20 @@
+package com.odnman.calculator;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b
+    }
+
+    public int sub(int a, int b) {
+        return a  - b
+    }
+
+    public int prod(int a, int b) {
+        return a * b
+    }
+
+    public int div(int a, int b) {
+        
+    }
+}
---
+++ src/test/java/com/odnman/CalculatorTest.java
@@ -0,0 +1,21 @@
+package com.odnman.calculator;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import com.odnman.calculator.Calculator
+
+/**
+ * Unit test for simple App.
+ */
+public class CalculatorTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
